7033a8f5a6a314a1912072f80f3118f005cdc7c4
GeorgeMarkham <george@gmarkham.com>
2021-05-16 21:33:55 +0100

    add public shared instance

--- Sources/FirebaseCombine/FirebaseCombine.swift
+++ Sources/FirebaseCombine/FirebaseCombine.swift
@@ -1,6 +1,8 @@
 import Firebase
 
 public struct FirebaseCombine {
+    public static let shared: FirebaseCombine = .init()
+    
     public func initialize() {
         FirebaseApp.configure()
     }
